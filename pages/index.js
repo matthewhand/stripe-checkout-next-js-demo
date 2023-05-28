bb23ddda86564f8ba886c2b28ad243ef4292122a
matthewhand <matthewhandau@gmail.com>
2023-05-28 15:35:59 +1000

    Update image URL

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
     name: "Apple AirPods",
     description: "Latest Apple AirPods.",
     image:
-      "https://images.unsplash.com/photo-1572569511254-d8f925fe2cbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+      "https://github.com/matthewhand/stripe-payment/raw/main/human_ai_conflict.png",
     quantity: 0,
     price: 999,
   });
